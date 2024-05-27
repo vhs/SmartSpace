29a2ac5c6f73c7fffb08b82507315ff204632996
Ty Eggen <tycho.eggen@gmail.com>
2024-05-27 01:36:49 +0000

    tools: change docker-compose to docker compose

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$(realpath "$0")")/" || exit 255
 
 echo "$(date): Checking vhs-smartspace..."
 
-docker-compose up --remove-orphans -d 2>&1 | while read -r DCLOG; do
+docker compose up --remove-orphans -d 2>&1 | while read -r DCLOG; do
     echo "$(date): ${DCLOG}"
 done
 
